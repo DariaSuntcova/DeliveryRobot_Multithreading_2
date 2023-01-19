8c4f65d4802ce5bdce61f7d0fc294f68b5c074fd
Daria Suntcova <batareyka23@gmail.com>
2023-01-19 14:58:28 +0300

    add new thread for search max count

--- src/javaa/main/Main.java
+++ src/javaa/main/Main.java
@@ -9,10 +9,12 @@ public class Main {
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
     public static final char R = 'R';
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        List<Thread> threads = new ArrayList<>();
 
         for (int i = 0; i < 1000; i++) {
-            new Thread(() -> {
+            Thread thread = new Thread(() -> {
                 String route = DeliveryRobot.generateRoute("RLRFR", 100);
                 char[] chars = route.toCharArray();
                 int countR = 0;
@@ -24,12 +26,43 @@ public class Main {
                 synchronized (sizeToFreq) {
                     if (countR > 0 & sizeToFreq.containsKey(countR)) {
                         sizeToFreq.put(countR, sizeToFreq.get(countR) + 1);
+                        sizeToFreq.notify();
                     } else {
                         sizeToFreq.put(countR, 1);
+                        sizeToFreq.notify();
                     }
                 }
-            }).start();
+            });
+            thread.start();
+            threads.add(thread);
         }
+
+        Thread searchMax = new Thread(() -> {
+            while (!Thread.interrupted()) {
+                synchronized (sizeToFreq) {
+                    try {
+                        Map.Entry<Integer, Integer> entry = sizeToFreq.entrySet().stream()
+                                .max(Map.Entry.comparingByValue()).get();
+                        print(entry, true);
+
+                        sizeToFreq.wait();
+
+                    } catch (NoSuchElementException exception) {
+                        System.out.println("Пока пусто");
+                    } catch (InterruptedException e) {
+                        return;
+                    }
+                }
+            }
+        });
+        searchMax.start();
+
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        searchMax.interrupt();
 
         LinkedHashMap<Integer, Integer> newMap = sizeToFreq.entrySet().stream()
                 .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
@@ -41,14 +74,19 @@ public class Main {
         boolean first = true;
 
         for (Map.Entry<Integer, Integer> entry : newMap.entrySet()) {
-            if (first) {
-                System.out.println("Самое частое количество повторений " + entry.getKey() + " (встретилось "
-                        + entry.getValue() + " раз)");
-                System.out.println("Другие размеры:");
-                first = false;
-            } else {
-                System.out.println("- " + entry.getKey() + " (" + entry.getValue() + " раз)");
-            }
+            print(entry, first);
+            first = false;
+        }
+    }
+
+    public static void print(Map.Entry<Integer, Integer> entry, boolean first) {
+        if (first) {
+            System.out.println("Самое частое количество повторений " + entry.getKey() + " (встретилось "
+                    + entry.getValue() + " раз)");
+            System.out.println("Другие размеры:");
+        } else {
+            System.out.println("- " + entry.getKey() + " (" + entry.getValue() + " раз)");
         }
     }
 }
+
